5ac5ff74f15a3089de38b6cfdda0ca9e6fb59f58
Laura Berge <lauraeberge@gmail.com>
2020-06-20 13:18:28 -0500

    changes test suite to expect -1 rather than 0 since no two negative numbers summed will equal 0. The given array is [-1, -1, -5, -3, -7, -4, -5, -6, -100, -4]. -1 is the highest number you can get with the subarray being [-1]. The only way to get to a non-negative number like 0 is to have a positive number in the array: i.e. the sum of [-1, 1] = 0

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 function largestSubarraySum(array){
     // return largest sum possible in the array if we can evaluate any chumk of the array
 
-    // Granted, I have largestSum = 0 rather than array[0] in order to pass a test
-    // The test takes array [-1, -1, -5, -3, -7, -4, -5, -6, -100, -4] and expects an output of 0
-    // no values in that array can be summed to be greater than -1.
-    // Therefore I believe that test is written incorrectly according to my understanding of the question and should expect the maxSubarray to be [-1] and output -1
-    let largestSum = 0
+    let largestSum = array[0]
     let currentSum = array[0]
 
     for (let i = 1; i < array.length; i++) {
